d82d73f31e419c367b3563dbd9efc9b776ae869f
Priyanka Kalena <36067394+pgk612@users.noreply.github.com>
2019-10-19 13:11:46 +0530

    Update changepass.php

--- changepass.php
+++ changepass.php
@@ -1,141 +1,210 @@
+<!-- background image not seen -->
 <?php
 session_start();
 require("functions.php");
-
-
-   
+ 
 ?>
 
 
-
-<html>
+<!DOCTYPE html>
+<!--
+Template Name: Basend
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html lang="">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" type="text/css" href="design_forms.css"> 
+<title>Record Book</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <script>
+<script>
         function validate(){
 
-            var a = document.getElementById("newpwd").value;
-            var b = document.getElementById("cpwd").value;
-            if (a!=b) {
-               alert("Passwords do no match");
-               return false;
-            }
-        }
+var a = document.getElementById("newpwd").value;
+var b = document.getElementById("cpwd").value;
+if (a!=b) {
+
+swal({
+title: 'Try Again!',
+text: 'Passwords do not match!',
+icon: 'error',
+})
+
+   return false;
+}
+else
+{
+if(a.length<8)
+{
+  swal({
+title: 'Password length too small!',
+text: 'The length of the password must be greater than 8 characters',
+icon: 'error',
+})
+return false;
+}
+else
+{
+  return true;
+}
+}
+
+return true;
+
+
+}
      </script>
+	 <style>
+	 
+footer{
 
-
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   #A2B70D
+   text-align: center;
+   height:max-content;
+}
+	 </style>
 </head>
-<body style="background-color:#002664;">
-
-<div class="flex-container">
-<header>
-  <h1><img   height="85" width="90" src="assets/logo1.png">
-
-  Somaiya Vidyavihar
-<img   height="85" width="90" src="assets/logo2.png"></h1>
-
-</header>
-
-<div class="container">
-  
-  <a href="home.php">My Account</a>
-  <a href="student_analysis.php">Analysis</a>
-  <div class="dropdown">
-      <b> <button class="dropbtn">Add New Activity <i class="fa fa-caret-down"></i></button></b>
-    <div class="dropdown-content">
-      <a  style="padding-top: 19px;"  href="workshop.php" >Workshops</a></hr>
-  <hr><a href="course.php">Courses</a></hr>
-  <hr><a href="tpp.php">TPP</a></hr>
-  <hr><a href="sports.php">Sports</a></hr>
-  <hr><a href="competition.php">Competitions</a></hr>
-<hr><a  style="padding-bottom: 19px;" href="other_activity.php">Other Activities</a></hr>
-
-      
-</div>
-  </div>
- <a href="viewprogress.php">View Progress</a>
-
- <a href="contact.html">Contact Us</a>
-
-
-
-
-<div class="clock">
-    <div class="dropdown">
-    <button class="dropbtn">    Hi, 
-    	 <?php echo $_SESSION['username'] ; ?>
-
-
-	<i class="fa fa-caret-down"></i>  
-    
-    </button>
-    <div class="dropdown-content">
-      <a href="changepass.php">Change password</a>
-       <hr>  <a href="index.html">Log Out</a></hr>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" > 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+      <h1>
+            <img   class ="logo2"  height="80" width="80" src="images/demo/logo2.png">   
+            <a href="#">Departmental Record Book</a>            <img   class ="logo1"  height="80" width="80" src="images/demo/logo1.png"> 
+          </h1>  </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="home.php">Home</a></li>
+          <li><a class="drop" href="#">Add Activity</a>
+            <ul>
+              <li><a href="workshop.php">Workshop</a></li>
+              <li><a href="course.php">Course</a></li>
+              <li><a href="tpp.php">TPP</a></li>
+              <li><a href="competition.php">Competition</a></li>
+              <li><a href="sports.php">Sports</a></li>
+              <li><a href="other_activity.php">Other Activity</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">View Progress</a>
+            <ul>
+              <li><a href="viewprogress/my_workshop.php">Workshop</a></li>
+              <li><a href="viewprogress/my_course.php">Course</a></li>
+              <li><a href="viewprogress/my_tpp.php">TPP</a></li>
+              <li><a href="viewprogress/my_competition.php">Competition</a></li>
+              <li><a href="viewprogress/my_sports.php">Sports</a></li>
+              <li><a href="viewprogress/my_other.php">Other Activity</a></li>
+            </ul>
+          </li>
+            <li><a class="drop" href="#">Analysis</a>
+            <ul>
+              <li><a href="student_analysis.php">Tabular</a></li>
+              <li><a href="student_graphical.php">Graphical</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Other</a>
+            <ul>
+              <li><a class="drop" href="#">View</a>
+              <ul>
+                  <li><a href="php/view_workshop.php">Workshop</a></li>
+                  <li><a href="php/view_course.php">Course</a></li>
+                  <li><a href="php/view_tpp.php">TPP</a></li>
+                   <li><a href="php/view_competition.php">Competition</a></li>
+                  <li><a href="php/view_sports.php">Sports</a></li>
+                  <li><a href="php/view_otheractivity.php">Other Activity</a></li>
+                </ul>
+                </li>
+           
+              <li><a href="changepass.php">Change Password</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+         
      
-    </div>
-  </div> 
-   </div>
-
-
-</div>
-</div>
-
-<div class="sidenav">
-	 <a  class ="report" href="home.php">Report <i class="fa fa-caret-down"></i></a>
-  <hr><a href="php/view_workshop.php">Workshops</a></hr>
-  <hr><a href="php/view_courses.php">Courses</a></hr>
-  <hr><a href="php/view_tpp.php">TPP</a></hr>
-  <hr><a href="php/view_sports.php">Sports</a></hr>
-  <hr><a href="php/view_competitions.php">Competitions</a></hr>
-<hr><a href="php/view_otheractivity.php">Other Activities</a></hr>
-
-
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
   </div>
 
+<div >
+  <main class="hoc container clear"> 
+  <div class="sectiontitle">
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+ 
+  
+      <h6 class="heading">CHANGE PASSWORD</h6>
 
-<div class="container3">
-<center>
-  <fieldset>
+    <div class="insert_form">
+  <form  onSubmit="return validate()" action="php/changepassword.php" method="POST" >
 
-    <legend><u>Enter password</u></legend>
+  <label for="old_pwd">Enter Current password:</label>
+    <input type="password" id="old_pwd" name="old_pwd" required >
+    <label for="newpwd">Enter new password:</label>
+    <input type="password" id="newpwd" name="newpwd" required >
 
-  <form  onSubmit="return validate()" action="php/changepassword.php" method="POST"">
-    <table>
+    <label for="cpwd">Re-enter password:</label>
+    <input type="password" id="cpwd" name="cpwd" required>
 
-<!--   <tr>
-    <td>Enter old password:</td>
-    <td><input type="password" name="oldpwd" id="oldpwd"  required></td>
-  </tr>
-  <tr> -->
+    <input type="submit" value="Confirm" name="btn">
+  </form>
+</div>
+    </div>
 
-  <tr>
-    <td>Enter new password:</td>
-    <td><input style="color:white" type="password" name="newpwd" id="newpwd"  required></td>
-  </tr>
-  <tr>
-    <td>Confirm password:</td>
-    <td><input style="color:white" type="password" name="cpwd"  id="cpwd" required></td>
-  </tr>
 
-</table>
-<br><br>    
-<input type="submit" value="Confirm">
-</form>
-</fieldset>
+
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    
+  </main>
+  <center>
+ <footer>
+<h3 style="margin-top:5px;"> For any further queries Email us at admin@somaiya.edu</h3>
+<a href="feedback_s.php" style="font-size: 20px;margin-top:-5px;">Click here for feedback / queries </a>
+</footer>
 
 </center>
+</div>
 
-    </div>
+</div><!--  background image tag -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
 
 
-
-
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
 </body>
-
-
 </html>
